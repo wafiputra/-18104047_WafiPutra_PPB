91be3d73759c38f9b9f0b577bf823d326601a405
wafiputra <wafi.putra.arya@gmail.com>
2022-01-25 23:45:10 +0700

    Modul Praktikum 10

---
+++ Modul 10/app/src/main/java/com/wafi_18104047/modul10/data/Quote.kt
@@ -0,0 +1,12 @@
+package com.wafi_18104047.modul10.data
+
+import android.os.Parcelable
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
